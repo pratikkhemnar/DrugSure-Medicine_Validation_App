f5c7426f67d3d8797c6bb769d5e65c3e0d894b81
Vaibhav05-code <vaibhavslokhande2004@gmail.com>
2025-11-27 14:24:47 +0530

    Add doctor consultancy screens and update routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
+// lib/main.dart
 import 'package:drugsuremva/E-commers%20Screen/E_HomeScreen.dart';
 import 'package:drugsuremva/E-commers%20Screen/providers/user_provider_screen.dart';
 import 'package:drugsuremva/auth/check_user_status_Screen.dart';
 import 'package:drugsuremva/auth/createAccount.dart';
-import 'package:drugsuremva/auth/splashScreen.dart';
 import 'package:drugsuremva/screens/mainhomeScreen.dart';
+import 'package:drugsuremva/doctor_consultancy/screens/doctor_list_screen.dart';
+import 'package:drugsuremva/doctor_consultancy/screens/video_call_screen.dart';
+import 'package:drugsuremva/auth/login.dart';
+import 'package:drugsuremva/auth/auth_wrapper.dart'; 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'auth/auth_wrapper.dart';
-import 'auth/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();  // Make sure binding is initialized
@@ -33,14 +34,15 @@ class MyApp extends StatelessWidget {
           useMaterial3: true
         ),
         routes: {
-          "/login":(context) => Login(),
-          "/signup": (context) =>Createaccount(),
-          "ehome":(context) => EHomescreen(),
-          "/mainhomescreen" : (context) => Homescreen()
-
+          "/login": (context) => Login(),
+          "/signup": (context) => Createaccount(),
+          "ehome": (context) => EHomescreen(),
+          "/mainhomescreen": (context) => Homescreen(),
+          "/doctor-list": (context) => DoctorListScreen(),
+          // add other routes here as needed
         },
         debugShowCheckedModeBanner: false,
-        home: SplashScreen(),   // add const if LoginScreen is const constructor
+        home: Login(),
       ),
     );
   }
